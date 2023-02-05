a8d11cc332bc1b8cf8b4bd7c7f19b3f016af5033
Sri Hari <kataresrihari@gmail.com>
2023-02-05 21:05:13 +0530

    added time

--- SET_2--1.py
+++ SET_2--1.py
@@ -3,7 +3,8 @@
 # describe_restaurant() that prints these two pieces of information, and a method
 # called open_restaurant() that prints a message indicating that the restaurant is
 # open. Create three different instances from the class, print the two attributes
-# individually, and then call both methods for each instance.'''
+# individually, and then call both methods for each instance.
+from datetime import datetime
 class Restaurant:
     def __init__(self,restaurant_name,cuisine_type):
         self.restaurant_name=restaurant_name
@@ -12,7 +13,11 @@ class Restaurant:
         print(f"Welcome to {self.restaurant_name}")
         print(self.cuisine_type)
     def open_restaurant(self):
-        print("Restaurant Open")
+        time = datetime.now().hour
+        if time>=11 and time<=23:print("Restaurant Open")
+        else:print("Restaurant Closed")
+
+
 a=input("Enter the Restaurant Name: ")
 b=input("Enter the Cuisine: ")
 R=Restaurant(a,b)
